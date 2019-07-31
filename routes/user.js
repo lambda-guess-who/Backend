202acccba6d5c70b838c934b3ca3375daca77108
Cai Martin <cmartin.professional@yahoo.com>
2019-07-31 14:57:41 -0400

    added highscore update

--- routes/user.js
+++ routes/user.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models/user");
 
-router.post("/highscore/:id", async (req, res) => {
+router.put("/highscore/:id", async (req, res) => {
   let { id } = req.params;
   try {
     let user = await User.findByIdAndUpdate(
